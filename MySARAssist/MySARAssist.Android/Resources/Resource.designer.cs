e98b3b1380bdbca2c2f9157e680eb905a2c6db5f
dylancbaker <dylan@greathat.ca>
2021-06-05 02:55:55 -0700

    Minor changes to finish the publish process

--- MySARAssist/MySARAssist.Android/Resources/Resource.designer.cs
+++ MySARAssist/MySARAssist.Android/Resources/Resource.designer.cs
@@ -17000,9 +17000,6 @@ namespace MySARAssist.Droid
 			// aapt resource value: 0x7F07009A
 			public const int tooltip_frame_light = 2131165338;
 			
-			// aapt resource value: 0x7F07009B
-			public const int xamarin_logo = 2131165339;
-			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
